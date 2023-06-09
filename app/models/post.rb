8b830f53d82a37ee4851f734c36bb283f924737d
theophilus doku <theodoku89@gmail.com>
2023-06-09 09:51:04 +0000

    Mofidy post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,23 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', counter_cache: true
-  validates_presence_of :author
+  validates :author, presence: true
   has_many :likes, foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
 
-  after_create :increment_author_posts_counter
-  after_destroy :decrement_author_posts_counter
-
-  def recent_comments(limit = 5)
-    comments.order(created_at: :desc).limit(limit)
-  end
+  after_save :update_author_posts_counter
 
   private
 
-  def increment_author_posts_counter
-    author.increment!(:posts_counter)
+  def update_author_posts_counter
+    author.update(posts_counter: author.posts.count)
   end
 
-  def decrement_author_posts_counter
-    author.decrement!(:posts_counter)
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
