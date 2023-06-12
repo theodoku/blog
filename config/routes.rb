65d73d08ff652ba3d0296f1945c23b38a143486a
theophilus doku <theodoku89@gmail.com>
2023-06-12 13:26:05 +0000

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "articles#index"
-
-  get "/articles", to: "articles#index"
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  get '/users', to: 'users#index', as: 'users' 
+  get '/users/:id', to: 'users#show', as: 'user'
 end
