15900a0f2b036793f25481b0a9a46ebef495ff31
theophilus doku <theodoku89@gmail.com>
2023-06-15 11:16:31 +0000

    fix linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
-    @post = Post.new 
+    @post = Post.new
   end
 
   def show
