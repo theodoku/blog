af57e87a89f3b7b4ccce4cce95afa3a4de93b15b
theophilus doku <theodoku89@gmail.com>
2023-06-20 11:10:40 +0000

    Create integration spec for views

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda/matchers'
+require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -64,5 +65,13 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
   end
 end
