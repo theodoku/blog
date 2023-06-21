bc2150452fc25edc12ce6acd76ce1fc377f16395
theophilus doku <theodoku89@gmail.com>
2023-06-21 16:16:33 +0000

    fix linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
- 
+
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
