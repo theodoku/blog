c29d596eb1d9b45a6080e6028d68cfea2e2731bb
theophilus doku <theodoku89@gmail.com>
2023-06-21 16:04:34 +0000

    solve registration session issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: :author_id
@@ -10,8 +10,8 @@ class User < ApplicationRecord
 
   before_save :update_posts_counter
 
-  validates :name, :photo, :bio, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
  
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
