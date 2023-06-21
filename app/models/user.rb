0a99dacdc2d79f8a98432d04232f867cba107b0d
theophilus doku <theodoku89@gmail.com>
2023-06-21 08:57:35 +0000

    Add devise to my blog app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,25 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: :author_id
+  has_one_attached :photo
 
-  before_save :update_posts_count
+  before_save :update_posts_counter
 
   validates :name, :photo, :bio, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
-  attr_accessor :photo, :bio, :email
-
+ 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
 
   private
 
-  def update_posts_count
+  def update_posts_counter
     self.posts_count = posts.count
   end
 end
